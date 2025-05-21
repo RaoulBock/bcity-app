84f180df9315d063f2dd3b2eaf3be25ae1dc9f95
Raoul Bock <anthonyraoulbock01@gmail.com>
2025-05-21 22:14:45 +0200

    'changes'

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ title }) => {
+  return (
+    <div className="button-wrapper">
+      <button className="create-client-btn">Create Client</button>
+    </div>
+  );
+};
+
+export default Button;
--- src/WIndows/ClientWindow.js
+++ src/WIndows/ClientWindow.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Nav from "../Components/Nav/Nav";
 import MainTab from "../Components/Tabs/MainTab";
+import Table from "../Components/Tables/Table";
+import Button from "../Components/Button/Button";
 
 const ClientWindow = () => {
+  const headers = ["Name", "Email", "Role"];
+  const data = [
+    { name: "Alice", email: "alice@example.com", role: "Admin" },
+    { name: "Bob", email: "bob@example.com", role: "User" },
+  ];
   return (
     <>
       <Nav title={"Client"} />
+      <div className="center-design-container">
+        <Button title={"Create client"} />
+        <Table headers={headers} data={data} />
+      </div>
     </>
   );
 };
